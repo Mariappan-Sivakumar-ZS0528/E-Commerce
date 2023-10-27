7d3f7cc592a461cdd4479b68356ff1a4dcd20ee7
Mariappan Sivakumar <mariappan.sivakumar@zucisystems.com>
2023-10-27 18:08:34 +0530

    Admin registeration added

--- src/main/java/com/app/shopping/ecommerce/controller/AuthController.java
+++ src/main/java/com/app/shopping/ecommerce/controller/AuthController.java
@@ -1,13 +1,13 @@
 package com.app.shopping.ecommerce.controller;
 
+import com.app.shopping.ecommerce.payload.AdminRegistrationDto;
 import com.app.shopping.ecommerce.payload.JWTAuthResponse;
 import com.app.shopping.ecommerce.payload.LoginDto;
 import com.app.shopping.ecommerce.services.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,5 +25,13 @@ public class AuthController {
         JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
         jwtAuthResponse.setToken(token);
         return ResponseEntity.ok(jwtAuthResponse);
+    }
+    @PostMapping("/admin/register")
+    public ResponseEntity<AdminRegistrationDto> registerAdmin(@RequestBody AdminRegistrationDto adminRegistrationDto){
+        return ResponseEntity.ok(authService.registerAdmin(adminRegistrationDto));
+    }
+    @GetMapping("/admin/all")
+    public ResponseEntity<List<AdminRegistrationDto>> getAllAdmins(){
+        return ResponseEntity.ok(authService.getAllAdmins());
     }
 }
