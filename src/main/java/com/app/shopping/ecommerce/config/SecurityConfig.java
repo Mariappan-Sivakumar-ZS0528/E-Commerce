1cf506d9592a174db65d2e0c7fe1691f859e75bd
Mariappan Sivakumar <mariappan.sivakumar@zucisystems.com>
2023-12-12 14:19:47 +0530

    remove problems

--- src/main/java/com/app/shopping/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/app/shopping/ecommerce/config/SecurityConfig.java
@@ -18,14 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-
-import java.util.Collections;
 
 @Configuration
 @EnableMethodSecurity
