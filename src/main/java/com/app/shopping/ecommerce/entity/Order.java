9260cae68b242b3e911034e02586f917de4f57f8
Rajkumar-Selvaraj <148527647+Rajkumar-Selvaraj@users.noreply.github.com>
2023-12-12 10:27:22 +0530

    Update Order.java

--- src/main/java/com/app/shopping/ecommerce/entity/Order.java
+++ src/main/java/com/app/shopping/ecommerce/entity/Order.java
@@ -1,30 +1,28 @@
-//package com.app.shopping.ecommerce.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.util.Date;
-//import java.util.List;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name = "orders")
-//public class Order {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String orderBy;
-//    private String Location;
-//    private int items;
-//    private double price;
-//    private Date placedOn;
-//    private Date deliveryOn;
-//    private String status;
-//    private List<Product> products;
-//}
+package com.app.shopping.ecommerce.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String orderBy;
+private String Location;
+private int items;
+private double price;
+private Date placedOn;
+private Date deliveryOn;
+private String status;
+@OneToMany(mappedBy = "order",cascade = CascadeType.ALL, orphanRemoval = true)
+private List<Product> products;
+}
